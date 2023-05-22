bcce7454e085d4f781473f525ade5b80cdbe09bd
Praveen Tewatia <praveen@Praveens-MacBook-Pro.local>
2023-05-22 20:06:21 +0530

    url updated

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -19,7 +19,7 @@ const Page = () => {
     }
     e.preventDefault();
     try {
-      const res = await fetch("/api/auth/login", {
+      const res = await fetch(`${process.env.URL}/api/auth/login`, {
         method: 'POST',
         headers: {
           "Content-Type": 'application/json'
